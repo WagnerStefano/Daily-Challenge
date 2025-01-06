de33606125d431126e0321b1cf028e2a00e908db
Wagner Stefano <wagnerstefano98@hotmail.com>
2025-01-06 00:22:17 -0300

    Create MyFavoriteMonster.py
    2° Desafio, criando um índice com informações sobre os Monstros que serão usados em um RPG, utilizando uma estrutura simples onde utilizamos Strings e Int, treinando sequencias de daos e futuramente melhorando o código implementando o While para validar os valores de Força e um comparador de Monstros para simular uma Batalha em turnos onde poderemos comparar os Atributos Força e Poder das Habilidades especiais que receberão efeitos bônus com base na Arena e tipo.

---
+++ 2° Desafio/MyFavoriteMonster.py
@@ -0,0 +1,6 @@
+nome = input("Insirá o Nome do Monstro: ")
+tipo_monstro = input ("Insirá o Atributo do Monstro: ")
+nivel_de_forca = int (input("Insira a Força de seu Monstro (1- 100): "))
+habilidades_especiais = input ("Insira a Habilidade Especial de seu Monstro: ")
+
+print  (f"O monstro {nome} do tipo {tipo_monstro} tem força {nivel_de_forca} e a habilidade de {habilidades_especiais}.")
