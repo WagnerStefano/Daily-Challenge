1f15674517df7c5f0a9a1b23c8beba19e310a121
Wagner Stefano <wagnerstefano98@hotmail.com>
2025-01-07 23:19:56 -0300

    V2
    dando andamento a versão 2.0 implementando POSTGRESQL e melhorando o RPG

--- 3° Desafio/Duelos_RPG.py
+++ 3° Desafio/Duelos_RPG.py
@@ -9,16 +9,16 @@
 #Por Exemplo: Might Knight, Cavaleiro, Habilidade Glorious Burst, Vida 98, Poder de Ataque 75, Defesa 32.
 
 personagens = [
-    {"nome": "Might Knight", "classe": "Cavaleiro", "habilidade": "Glorious Burst", "vida": 98, "poder de Ataque" 75, "Defesa" 32},
-    {"nome": "", "classe": "", "habilidade": "", "vida": , "poder de Ataque": , "Defesa": },
-    {"nome": "", "classe": "", "habilidade": "", "vida": , "poder de Ataque": , "Defesa": },
-    {"nome": "", "classe": "", "habilidade": "", "vida": , "poder de Ataque": , "Defesa": },
+    {"nome: " "Might Knight", "classe: " "Cavaleiro", "habilidade: " "Glorious Burst", "vida: " "98", "poder de Ataque" "75", "Defesa" "32"},
+    {"nome:" "Golden Potato", "classe:" "Tuberculo Mutante", "habilidade:" "Amido Acido", "vida: " "100", "poder de Ataque: ""35" , "Defesa:""50" },
+    {"nome:" "Colossu", "classe: " "Gigante", "habilidade: " "Esbarrão", "vida:" "99", "poder de Ataque:" "62", "Defesa:" "80"},
+    {"nome:" "Austalopithecus", "classe: " "Hominideo", "habilidade: " "Primeira Chama", "vida:" "60" , "poder de Ataque:" "72" , "Defesa:" "50" },
 ]
 
 def selecione_um_personagem(personagens):
     print ("Escolha seu personagem:")
     for i, personagem in enumerate(personagens):
-        print(f"{i+1}. {personagem[nome]}")
+        print(f"{i+1}. {personagem["nome"]}")
 
     escolha = int(input("Digite o número do personagem: ")) -1
     return personagens [escolha]
