b42598d19954b76c982b1ef1ec26c53fc7ea37dd
Wagner Stefano <wagnerstefano98@hotmail.com>
2025-01-10 23:51:27 -0300

    Create sequencia_fibonacci.py

---
+++ Fibonacci/sequencia_fibonacci.py
@@ -0,0 +1,11 @@
+print("-"*30)
+print("Sequência de Fibonacci")
+print("-"*30)
+
+n = int(input("De Quantos termos você precisa? "))
+t1 = 0
+t2 = 1
+
+print ("{}=>{}".format(t1, t2))
+t3 = t1 + t2
+print("=>{}".format(t3))
