3dfd6d703f0f6e4d3622f1cbd4792585e41c411e
Wagner Stefano <wagnerstefano98@hotmail.com>
2025-01-14 18:39:40 -0300

    4° Dayli

---
+++ 4°Desafio/MonsterBuilder.py
@@ -0,0 +1,9 @@
+
+
+name = input("Insira o nome do seu monstro: ")
+element = input("Insira o Elemento do seu monstro: ")
+health = int(input("Insira os pontos de vida do seu monstro(1 a 100): "))
+skills = input("Informe as habilidades do seu monstro: ")
+
+print ("Monstro criado com sucesso, aqui esta")
+print(f"{name}, pertencendo ao elemento {element}, com {health} pontos devida e habilidades {skills}")
